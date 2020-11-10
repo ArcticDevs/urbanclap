a7cdf63122139a562e66c2b94bfab705efe5d38f
yugchandak <yugchandak1@gmail.com>
2020-11-10 20:17:47 +0530

    footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="footer container-fluid">
+    <div class="container">
+      <div id="linksContainer">
+        <router-link to="/" id="links"> About Us </router-link>
+        <router-link to="/" id="links"> Terms & condition </router-link>
+        <router-link to="/" id="links"> Privacy Policy </router-link>
+        <router-link to="/" id="links"> Blog </router-link>
+        <router-link to="/" id="links">Reviews </router-link>
+        <router-link to="/" id="links"> Near Me </router-link>
+        <router-link to="/" id="links"> Careers </router-link>
+        <router-link to="/" id="links">Gift Cards </router-link>
+        <router-link to="/" id="links"> Contact Us </router-link>
+      </div>
+      <div id="Servein">
+        <p class="text-white">Serving In</p>
+        <p class="text-white">AUS</p>
+        <router-link to="/" id="links" > Sydney </router-link>
+
+        <p class="text-white mt-3 ">IND</p>
+        <div id="linksContainerIND">
+          <router-link to="/"> <p>Ahmedabad</p></router-link>
+          <router-link to="/"> <p>Bangolore</p></router-link>
+          <router-link to="/"> <p>Chennai</p></router-link>
+          <router-link to="/"> <p>Delhi NCR</p> </router-link>
+          <router-link to="/"> <p>Chandigarh Tricity</p></router-link>
+          <router-link to="/"> <p>Jaipur</p></router-link>
+          <router-link to="/"> <p>Hydrabad</p></router-link>
+          <router-link to="/"> <p>Kolkata</p></router-link>
+          <router-link to="/"> <p>Mumbai</p> </router-link>
+          <router-link to="/"> <p>Pune</p></router-link>
+          <router-link to="/"> <p>Nagpur</p></router-link>
+          <router-link to="/"> <p>Ludhiana</p></router-link>
+          <router-link to="/"> <p>Vishakhapatnam</p> </router-link>
+          <router-link to="/"> <p>Vadodra</p> </router-link>
+          <router-link to="/"> <p>Lucknow</p> </router-link>
+          <router-link to="/"> <p>Bhubaneswar</p> </router-link>
+          <router-link to="/"><p>Surat</p></router-link>
+          <router-link to="/"><p>Indore</p></router-link>
+        </div>
+        <div class="d-inline-block">
+          <p class="text-white ">UAE</p>
+          <div id="linksContainerIND">
+            <router-link to="/"> <p>Dubai</p></router-link>
+            <router-link to="/"> <p>Abu Dhabi</p></router-link>
+          </div>
+        </div>
+
+          <div class="d-inline-block ml-4">
+          <p class="text-white ">SGP</p>
+          <div id="linksContainerIND">
+            <router-link to="/"> <p>Sydney</p></router-link>
+      
+          </div>
+        </div>
+      </div>
+      <div class=" footer-end">
+      <div class="d-inline-block text-white w-auto" >
+        <div class="d-inline-block text-white w-auto" >
+         <img src="/logo_light.png" alt="" srcset="" id="logo" > 
+        </div>
+        <div class="d-inline-block text-white w-auto ml-2" >
+        <span style="font-size:12px; color:#cdcdcd"> ©2014-20 UrbanClap Techonologies India Pvt. Ltd.</span>
+        </div>
+      </div>
+      
+      <div class="d-inline-block w-auto mx-auto">
+       <i class="fab fa-twitter mr-2 text-white "></i>  <i class="fab fa-facebook-square mr-2 text-white "></i> <i class="fab fa-instagram mr-2 text-white "></i> <i class="fab fa-youtube mr-2 text-white "></i> <i class="fab fa-linkedin mr-2 text-white "></i> <i class="fab fa-pinterest-p mr-2 text-white "></i>
+      </div>
+      
+      <div class="d-inline-block w-auto">
+        <img src="/googleplay.png" alt="" srcset="" id="store">
+        <img src="/appstore.png" alt="" srcset="" id="store">
+      </div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container-fluid {
+  background-color: #000;
+}
+
+#links {
+  color: grey;
+  text-decoration: none;
+  margin-right: 20px;
+}
+#linksContainer {
+  text-align: left;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  border-bottom: 0.5px solid white;
+}
+
+#linksContainerIND > * {
+  text-align: left;
+  color: white;
+  margin-right: 25px;
+  text-decoration: none;
+  display: inline-block;
+}
+
+#Servein {
+  text-align: left;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  border-bottom: 0.5px solid white;
+}
+#logo{
+  width: 190px;
+  height: 20px;
+}
+#store{
+   width: 125px;
+  height: 40px;
+}
+*{
+  color: rgb(128, 117, 117);
+}
+.footer-end{
+  padding-top: 25px;
+  padding-bottom: 25px;
+
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  text-align: center;
+  align-items: center;
+
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faSpinner)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
